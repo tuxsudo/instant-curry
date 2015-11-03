70bd1933ffc946b21fafa52baa55257b73ac4509
Jared Anderson <jared@tuxsudo.com>
2015-11-03 11:34:53 -0700

    rebuild commonjs implementation

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,23 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = curry;
-function curry(fn, numArgs) {
+function curry(fn) {
 
     return function (arg) {
-        return (numArgs || fn.length) <= 1 ? fn.call(this, arg) : curry(fn.bind(this, arg), (numArgs || fn.length) - 1);
+
+        // invoked when only expecting one argument...
+        // or
+        // enought arguments where passed in to invoke the function
+        // return invoked value
+        if (fn.length <= 1 || arguments.length >= fn.length) {
+            return fn.apply(this, arguments);
+        }
+
+        // not enough arguments were provided
+        // return curried version of partially-applied function
+        return [].reduce.call(arguments, function (l, c) {
+            return curry(l.bind(null, c));
+        }, fn);
     };
 }
 
